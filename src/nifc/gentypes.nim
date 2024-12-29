b2afac72196aa02f9e3702a2b3b76f05d07f4dc3
metagn <metagngn@gmail.com>
2024-12-29 16:53:33 +0300

    make gear3 use the base type directly

--- src/nifc/gentypes.nim
+++ src/nifc/gentypes.nim
@@ -343,7 +343,7 @@ proc genEnumDecl(c: var GeneratedCode; t: TypeGraph; n: NodePos; name: string) =
   c.add Semicolon
   c.add NewLine
 
-  for ch in sonsFromX(t, n, 2):
+  for ch in sonsFromX(t, n):
     if t[ch].kind == EfldC:
       let (a, b) = sons2(t, ch)
       if t[a].kind == SymDef:
