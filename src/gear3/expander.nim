162e0fc0d77a8e24136906de3e058a7fc8d64312
metagn <metagngn@gmail.com>
2025-01-02 12:00:03 +0300

    use `(expr)` and `commonType` for expression proc bodies (#320)
    * fix expression proc bodies, use commonType
    
    * traverse generic proc param/typevar symbols in gear3

--- src/gear3/expander.nim
+++ src/gear3/expander.nim
@@ -613,11 +613,28 @@ proc traverseProc(e: var EContext; c: var Cursor; mode: TraverseMode) =
 
   if c.substructureKind == TypevarsS:
     isGeneric = true
-
-  skip c # generic parameters
+    # count each typevar as used:
+    inc c
+    while c.kind != ParRi:
+      assert c.symKind == TypevarY
+      inc c
+      let (typevar, _) = getSymDef(e, c)
+      e.offer typevar
+      skipToEnd c
+    inc c
+  else:
+    skip c # generic parameters
 
   if isGeneric:
-    skip c # skip parameters
+    # count each param as used:
+    inc c
+    while c.kind != ParRi:
+      assert c.symKind == ParamY
+      inc c
+      let (param, _) = getSymDef(e, c)
+      e.offer param
+      skipToEnd c
+    inc c
     skip c # skip return type
   else:
     traverseParams e, c
