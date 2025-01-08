10cee62bbbe2c1d2471212fbedb0c0c7b301059b
ringabout <43030857+ringabout@users.noreply.github.com>
2025-01-08 22:26:16 +0800

    fixes some issues

--- src/nimony/xints.nim
+++ src/nimony/xints.nim
@@ -116,7 +116,9 @@ proc `$`*(a: xint): string =
   else: $a.val
 
 proc createXint*(x: int64): xint =
-  if x < 0:
+  if x == low(int64):
+    xint(neg: true, val: cast[uint64](x))
+  elif x < 0:
     xint(neg: true, val: uint64(-x))
   else:
     xint(neg: false, val: uint64(x))
