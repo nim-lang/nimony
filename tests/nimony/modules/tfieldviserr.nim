f01363fada331825d22b73ac7e61de8e0de4a1cb
metagn <metagngn@gmail.com>
2025-01-10 04:18:26 +0300

    disable compilation test for now

--- tests/nimony/modules/tfieldviserr.nim
+++ tests/nimony/modules/tfieldviserr.nim
@@ -5,3 +5,17 @@ discard foo.private
 
 var generic = Generic[int](private: 123)
 discard generic.private
+
+when false: # compiles and should compile but cannot test yet
+  var foo = Foo(public: 123)
+  discard getPrivate(foo)
+  discard getPrivateTempl(foo)
+
+  var generic = Generic[int](public: 123)
+  discard getPrivate(foo)
+  discard getPrivateTempl(foo)
+
+  template resem() =
+    foo = Foo(public: 123)
+    generic = Generic[int](public: 123)
+  resem()
