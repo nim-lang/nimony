2b990aecc4aab133a01d9126e37e214ac225f8e5
ringabout <43030857+ringabout@users.noreply.github.com>
2025-01-16 22:13:50 +0800

    adds a test case

--- tests/nimony/sysbasics/tcompile.nim
+++ tests/nimony/sysbasics/tcompile.nim
@@ -1,5 +1,8 @@
 {.compile: "foo.c".}
+{.compile("foo.c", "-fno-strict-aliasing")
 
 proc myFunc(): int {.importc: "myFunc".}
+proc myFunc2(): int {.importc: "myFunc2".}
 
 let s = myFunc()
+let s2 = myFunc2()
