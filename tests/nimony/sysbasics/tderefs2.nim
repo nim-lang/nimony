1e35cebb60c23460fc231be9e85b39c6b11f7033
ringabout <43030857+ringabout@users.noreply.github.com>
2025-01-21 16:11:29 +0800

    tests

--- tests/nimony/sysbasics/tderefs2.nim
+++ tests/nimony/sysbasics/tderefs2.nim
@@ -6,3 +6,23 @@ proc foo =
   let x = g(s)
 
 foo()
+
+
+proc g1(x: var int): var int =
+  result = x
+
+proc g2(x: var int): var int =
+  result = x
+
+# proc g3[T](x: var T): T =
+#   result = x
+
+proc foo1(x: int) =
+  var x = x
+  var s = g1(x)
+  let y = g2(s)
+  let n = g2(g1(x))
+  # let s2 = g3(x)
+
+foo1(12)
+
