0393db580a4c02d2147c58b1d4dd013daac0eb3d
metagn <metagngn@gmail.com>
2025-02-11 00:22:05 +0300

    adapt (stmts) to getType

--- src/nimony/typenav.nim
+++ src/nimony/typenav.nim
@@ -108,6 +108,14 @@ proc getTypeImpl(c: var TypeCache; n: Cursor): Cursor =
         inc n
         skip n # label or DotToken
         result = getTypeImpl(c, n)
+      of StmtsS:
+        var n = n
+        inc n # skip "stmts"
+        while n.kind != ParRi:
+          let prev = n
+          skip n
+          if n.kind == ParRi:
+            result = getTypeImpl(c, prev)
       else:
         discard
     else:
