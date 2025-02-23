062209d3ad1c73623e3486fdb3678f4ff833b7c5
metagn <metagngn@gmail.com>
2025-02-23 11:57:05 +0300

    make compat untyped templates/generics opt in for now (#605)

--- tests/nimony/compat/ttemplate.nim
+++ tests/nimony/compat/ttemplate.nim
@@ -1,6 +1,6 @@
 proc printf(format: cstring) {.importc: "printf", varargs, header: "<stdio.h>", nodecl.}
 
-template foo(cond: untyped) =
+template foo(cond: untyped) {.untyped.} =
   let x = 123
   printf("%d\n", x)
   if cond:
