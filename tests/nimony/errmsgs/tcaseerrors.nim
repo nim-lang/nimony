216b863e2f6ff23213d91d6f566e0300ac0cf836
metagn <metagngn@gmail.com>
2025-03-03 22:42:06 +0300

    test evaluation errors

--- tests/nimony/errmsgs/tcaseerrors.nim
+++ tests/nimony/errmsgs/tcaseerrors.nim
@@ -2,14 +2,14 @@ let a = "abc"
 
 case a
 of "abc", "def": discard
-of "ghi", "jkl": discard
+of "ghi", "jkl", a: discard
 of "mno", "abc": discard
 
 let b = 123
 
 case b
 of 123, 456: discard
-of 789, 122..124: discard
+of 789, 122..124, b: discard
 of 456: discard
 else: discard
 
@@ -17,5 +17,5 @@ let c = cstring"abc"
 
 case c
 of cstring"abc", cstring"def": discard
-of cstring"ghi", cstring"jkl": discard
+of cstring"ghi", cstring"jkl", c: discard
 of cstring"mno", cstring"abc": discard
