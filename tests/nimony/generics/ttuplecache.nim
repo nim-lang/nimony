0856099e0471ca5b21b22aef8572a8a357dc174f
metagn <metagngn@gmail.com>
2025-03-05 21:19:36 +0300

    update tests, also test named tuples

--- tests/nimony/generics/ttuplecache.nim
+++ tests/nimony/generics/ttuplecache.nim
@@ -11,3 +11,14 @@ proc initFooInt(): Foo[int] =
 proc initFooTup(): Foo[(int, int)] =
   # Error: type mismatch
   Foo[(int, int)](data: (0, 0))
+
+proc initFooGeneric[T](x: T): Foo[T] =
+  Foo[T](data: x)
+
+let x = initFooGeneric((a: 1, b: "abc"))
+let x1: int = x.data.a
+let x2: string = x.data.b
+
+let y = initFooGeneric((x: "def", y: 2))
+let y1: string = y.data.x
+let y2: int = y.data.y
