c4f863757be8afeaa5c4c5d391d7eca2affcbb12
metagn <metagngn@gmail.com>
2025-03-06 07:08:54 +0300

    clean up

--- src/nimony/sigmatch.nim
+++ src/nimony/sigmatch.nim
@@ -52,7 +52,8 @@ type
     tvars: HashSet[SymId]
     fn*: FnCandidate
     args*, typeArgs*: TokenBuf
-    err*, flipped*, matchingArgType: bool
+    err*, flipped*: bool
+    concreteMatch: bool
     skippedMod: TypeKind
     argInfo: PackedLineInfo
     pos, opened: int
@@ -329,20 +330,25 @@ proc linearMatch(m: var Match; f, a: var Cursor; flags: set[LinearMatchFlag] = {
     if f.kind == Symbol and isTypevar(f.symId):
       # type vars are specal:
       let fs = f.symId
-      if not m.matchingArgType and m.inferred.contains(fs):
+      if m.concreteMatch:
+        # generic param is from provided argument type
+        # instead of considering inference, treat as a standalone value
+        if matchesConstraint(m, fs, a):
+          inc f
+          skip a
+        else:
+          m.error(ConstraintMismatch, f, a)
+          break
+      elif m.inferred.contains(fs):
         # rematch?
         var prev = m.inferred[fs]
-        # the inferred type can contain generic parameters,
-        # in which case we ignore what those generic parameters were inferred to,
-        # as they cannot belong to the typevars of the current match
-        m.matchingArgType = true
+        # mark that the match is to a given type from the outside context:
+        m.concreteMatch = true
         linearMatch(m, prev, a, flags) # skips a
-        m.matchingArgType = false
+        m.concreteMatch = false # was already false because of `elif`
         inc f
       elif matchesConstraint(m, fs, a):
-        if not m.matchingArgType:
-          # again, do not give values to generic parameters in the argument type
-          m.inferred[fs] = a # NOTICE: Can introduce modifiers for a type var!
+        m.inferred[fs] = a # NOTICE: Can introduce modifiers for a type var!
         inc f
         skip a
       else:
@@ -525,19 +531,18 @@ proc matchSymbol(m: var Match; f: Cursor; arg: Item) =
   let a = skipModifier(arg.typ)
   let fs = f.symId
   if isTypevar(fs):
-    if not m.matchingArgType and m.inferred.contains(fs):
+    if m.concreteMatch:
+      if not matchesConstraint(m, fs, a):
+        m.error ConstraintMismatch, f, a
+    elif m.inferred.contains(fs):
       # used to call typevarRematch
       var prev = m.inferred[fs]
-      # the inferred type can contain generic parameters,
-      # in which case we ignore what those generic parameters were inferred to,
-      # as they cannot belong to the typevars of the current match
-      m.matchingArgType = true
+      # mark that the match is to a given type from the outside context:
+      m.concreteMatch = true
       singleArgImpl(m, prev, arg)
-      m.matchingArgType = false
+      m.concreteMatch = false # was already false because of `elif`
     elif matchesConstraint(m, fs, a):
-      if not m.matchingArgType:
-        # again, do not give values to generic parameters in the argument type
-        m.inferred[fs] = a
+      m.inferred[fs] = a
     else:
       m.error ConstraintMismatch, f, a
   elif isObjectType(fs):
