6213567f398c82191e4b53d43d13842611d88269
metagn <metagngn@gmail.com>
2025-03-15 21:19:09 +0300

    try removing ExportMode, index saves idents

--- src/nimony/semdata.nim
+++ src/nimony/semdata.nim
@@ -69,13 +69,6 @@ type
   ModuleFlag* = enum
     IsSystem, IsMain, SkipSystem
 
-  ExportModeKind* = enum
-    ExportAll, FromExport, ExportExcept
-
-  ExportMode* = object
-    kind*: ExportModeKind
-    list*: HashSet[SymId] # `from export` or `export except` symbol list
-
   SemContext* = object
     dest*: TokenBuf
     routine*: SemRoutine
@@ -107,7 +100,7 @@ type
     hookIndexLog*: array[AttachedOp, seq[HookIndexEntry]] # only a log, used for index generation, but is not read from.
     converters*: Table[SymId, seq[SymId]]
     converterIndexMap*: seq[(SymId, SymId)]
-    exports*: OrderedTable[SymId, ExportMode] # module syms to export mode
+    exports*: OrderedTable[SymId, ImportFilter] # module syms to export filter
     freshSyms*: HashSet[SymId] ## symdefs that should count as new for semchecking
     toBuild*: TokenBuf
     unoverloadableMagics*: HashSet[StrId]
