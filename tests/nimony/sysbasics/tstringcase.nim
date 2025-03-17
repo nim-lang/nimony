e2fd8fd203b2a1c87d2a96858558cebb43d136c6
ringabout <43030857+ringabout@users.noreply.github.com>
2025-03-17 22:43:24 +0800

    hexer: implements case string (#800)

--- tests/nimony/sysbasics/tstringcase.nim
+++ tests/nimony/sysbasics/tstringcase.nim
@@ -1,11 +1,10 @@
 import std/syncio
 
 proc main() =
-  when false: # works in sem but not hexer
-    let a = "abc"
-    case a
-    of "def", "ghi": echo "wrong"
-    of "abc", "jkl": echo "correct"
-    else: echo "also wrong"
+  let a = "abc"
+  case a
+  of "def", "ghi": echo "wrong"
+  of "abc", "jkl": echo "correct"
+  else: echo "also wrong"
 
 main()
