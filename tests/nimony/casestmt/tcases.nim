6b3ac83ec759f6de5b559c36b4f17f8988fb48be
ringabout <43030857+ringabout@users.noreply.github.com>
2025-03-18 22:48:51 +0800

    nifcgen: supports of range branches (#809)
    * nifcgen: supports of range branches
    
    * Update tests/nimony/casestmt/tcases.nim
    
    ---------
    
    Co-authored-by: Andreas Rumpf <rumpf_a@web.de>

--- tests/nimony/casestmt/tcases.nim
+++ tests/nimony/casestmt/tcases.nim
@@ -7,3 +7,11 @@ proc classify(s: string) =
   else: discard
 
 classify("9123345")
+
+proc classify1(s: string) {.requires: s.len > 0.} =
+  case s[0]
+  of '_': echo "an identifier"
+  of '0' .. '9': echo "a number"
+  else: discard
+
+classify1("9123345")
