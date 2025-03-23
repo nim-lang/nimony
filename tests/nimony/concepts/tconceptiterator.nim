c1c9805b78ee30d91989c9b5212311ffdc937247
demotomohiro <gpuppur@gmail.com>
2025-03-23 20:17:03 +0900

    updates test code and data to pass tests

--- tests/nimony/concepts/tconceptiterator.nim
+++ tests/nimony/concepts/tconceptiterator.nim
@@ -1,6 +1,6 @@
 # issue #829
 
-iterator `..`*(a, b: int): int {.inline.} =
+iterator `....`*(a, b: int): int {.inline.} =
   var i = a
   while i <= b:
     yield i
@@ -8,11 +8,11 @@ iterator `..`*(a, b: int): int {.inline.} =
 
 type
   Iterable* = concept
-    iterator `..`(a, b: Self): Self
+    iterator `....`(a, b: Self): Self
 
 proc default2*[I: Iterable; T: HasDefault](x: typedesc[array[I, T]]): array[I, T] {.inline, noinit.} =
-  # Needs to call iterator `..`(a, b: I): I
-  for i in low(array[I, T]) .. high(array[I, T]):
+  # Needs to call iterator `....`(a, b: I): I
+  for i in low(array[I, T]) .... high(array[I, T]):
     result[i] = default(T)
 
 var x = default2(array[2, int])
