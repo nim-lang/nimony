525b224be8c386c90a7c281e1d12ea271598c572
demotomohiro <gpuppur@gmail.com>
2025-03-29 00:29:24 +0900

    implements countdown iterator

--- lib/std/system/iterators.nim
+++ lib/std/system/iterators.nim
@@ -9,3 +9,19 @@ iterator `..`*[T: Ordinal](a, b: T): T {.inline.} =
   while i <= b:
     yield i
     inc i
+
+iterator countdown*[T, V: Ordinal](a, b: T; step: V = T(1)): T {.inline.} =
+  ## Counts from ordinal value `a` down to `b` (inclusive) with the given
+  ## step count.
+  ##
+  ## `step` may only be positive.
+  ##
+  ## **Note**: This fails if `a >= b and b + step > high(T)` for
+  ## efficiency reasons and it is a rare case.
+  var res = a
+  if res >= b:
+    while true:
+      yield res
+      if res < succ(b, step):
+        break
+      dec(res, step)
