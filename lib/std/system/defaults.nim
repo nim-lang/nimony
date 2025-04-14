47342fa356a696c920abb044338594190ed8bc55
metagn <metagngn@gmail.com>
2025-04-14 18:13:40 +0300

    account for lack of #931

--- lib/std/system/defaults.nim
+++ lib/std/system/defaults.nim
@@ -16,7 +16,8 @@ template default*(x: typedesc[uint64]): uint64 = 0'u64
 template default*(x: typedesc[float32]): float32 = 0.0'f32
 template default*(x: typedesc[float64]): float64 = 0.0'f64
 template default*(x: typedesc[string]): string = ""
-template default*[T: enum](x: typedesc[T]): T = low(T)
+template default*[T: OrdinalEnum](x: typedesc[T]): T = low(T)
+template default*[T: HoleyEnum](x: typedesc[T]): T = low(T)
 
 template default*[T: ptr](x: typedesc[T]): T = T(nil)
 template default*[T: ref](x: typedesc[T]): T = T(nil)
