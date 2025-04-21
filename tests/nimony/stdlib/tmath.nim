e6e4289a1ab01e2692bad660b26ac6118b1b5230
demotomohiro <gpuppur@gmail.com>
2025-04-21 17:02:13 +0900

    signbit and classify procs can takes both float32 and float64

--- tests/nimony/stdlib/tmath.nim
+++ tests/nimony/stdlib/tmath.nim
@@ -1,9 +1,14 @@
 import std/[math, assertions]
 
+let
+  FLT_MIN {.importc: "FLT_MIN", header: "<float.h>".}: float32
+  DBL_MIN {.importc: "DBL_MIN", header: "<float.h>".}: float64
+
 assert not signbit(0.0)
 assert signbit(0.0 * -1.0)
 assert signbit(-0.1)
 assert not signbit(0.1)
+assert signbit(-1.0'f)
 
 assert classify(1.0) == fcNormal
 assert classify(-1.0) == fcNormal
@@ -14,3 +19,6 @@ assert classify(1.0 / 0.0) == fcInf
 assert classify(-1.0 / 0.0) == fcNegInf
 assert classify(Inf) == fcInf
 assert classify(NaN) == fcNan
+assert classify(DBL_MIN / 2.0) == fcSubnormal
+assert classify(1.0'f32) == fcNormal
+assert classify(FLT_MIN / 2.0'f32) == fcSubnormal
