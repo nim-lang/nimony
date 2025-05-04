b764da79de91a27052c218227ab3fc8eaf7c075d
metagn <metagngn@gmail.com>
2025-05-04 21:06:39 +0300

    even more

--- tests/nimony/when/twhenforwarddecl.nim
+++ tests/nimony/when/twhenforwarddecl.nim
@@ -10,10 +10,16 @@ when not defined(nimony):
     Foo = Bar
     Bar = int
   proc foo() = foo2()
-  proc foo2() = echo "abc"
+  proc foo2() =
+    echo "abc"
+    foo3()
 else:
   type
     Foo = Bar
     Bar = int
   proc foo() = foo2()
-  proc foo2() = echo "def"
+  proc foo2() =
+    echo "def"
+    foo3()
+
+proc foo3() = discard
