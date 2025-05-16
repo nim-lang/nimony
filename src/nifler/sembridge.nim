51483b9f7aa77ec8683b16260dfa740251b794f1
Araq <rumpf_a@web.de>
2025-05-16 16:14:07 +0200

    WIP: add IDE tooling to Nifler

---
+++ src/nifler/sembridge.nim
@@ -0,0 +1,93 @@
+#       Nifler
+# (c) Copyright 2024 Andreas Rumpf
+#
+# See the file "license.txt", included in this
+# distribution, for details about the copyright.
+
+## Implements the mapping from Nim AST -> NIF.
+
+when defined(nifBench):
+  import std / monotimes
+
+import std / [assertions, syncio, os]
+
+import compiler / [
+  ast, modulegraphs, modules, commands, options, pathutils, renderer, lineinfos,
+  syntaxes, llstream, idents, msgs, passes, sem, pipelines, reorder]
+
+import ".." / lib / nifbuilder
+import ".." / models / nifler_tags
+
+proc connectCallbacks(graph: ModuleGraph) =
+  graph.includeFileCallback = modules.includeModule
+  graph.importModuleCallback = passes.importModule
+
+proc semFile*(thisfile, outfile: string; portablePaths, depsEnabled: bool) =
+  var conf = newConfigRef()
+  var graph = newModuleGraph(newIdentCache(), conf)
+  #if not self.loadConfigsAndProcessCmdLine(cache, conf, graph):
+  #  return
+  connectCallbacks(graph)
+
+  if conf.cmd == cmdCheck and optWasNimscript notin conf.globalOptions and
+       conf.backend == backendInvalid:
+    conf.backend = backendC
+
+  if conf.selectedGC == gcUnselected:
+    if conf.backend in {backendC, backendCpp, backendObjc} or
+        (conf.cmd in cmdDocLike and conf.backend != backendJs) or
+        conf.cmd == cmdGendepend:
+      initOrcDefines(conf)
+
+  var p = default(Parser)
+  let fileIdx = fileInfoIdx(conf, AbsoluteFile thisfile)
+  var module = graph.getModule(fileIdx)
+
+  prepareConfigNotes(graph, module)
+  var idgen = idGeneratorFromModule(module)
+  let ctx = preparePContext(graph, module, idgen)
+  let bModule = PPassContext(nil)
+
+  var s: PLLStream
+  let filename = toFullPathConsiderDirty(graph.config, module.fileIdx)
+  s = llStreamOpen(filename, fmRead)
+  if s == nil:
+    rawMessage(graph.config, errCannotOpenFile, filename.string)
+    return
+  graph.interactive = false
+
+  syntaxes.openParser(p, module.fileIdx, s, graph.cache, graph.config)
+
+  if not belongsToStdlib(graph, module) or (belongsToStdlib(graph, module) and module.name.s == "distros"):
+    # XXX what about caching? no processing then? what if I change the
+    # modules to include between compilation runs? we'd need to track that
+    # in ROD files. I think we should enable this feature only
+    # for the interactive mode.
+    if module.name.s != "nimscriptapi":
+      processImplicitImports graph, graph.config.implicitImports, nkImportStmt, module, ctx, bModule, idgen
+      processImplicitImports graph, graph.config.implicitIncludes, nkIncludeStmt, module, ctx, bModule, idgen
+
+  checkFirstLineIndentation(p)
+  var n = parseTopLevelStmt(p)
+  var moduleStmts = newNodeI(nkStmtList, n.info)
+  if n.kind != nkEmpty:
+    # read everything, no streaming possible
+    var sl = newNodeI(nkStmtList, n.info)
+    sl.add n
+    while true:
+      var n = parseTopLevelStmt(p)
+      if n.kind == nkEmpty: break
+      sl.add n
+    prePass(ctx, sl)
+    if sfReorder in module.flags or codeReordering in graph.config.features:
+      sl = reorder(graph, sl, module)
+    let semNode = semWithPContext(ctx, sl)
+    #echo renderTree(semNode)
+    moduleStmts.add semNode
+
+  closeParser(p)
+  let finalNode = closePContext(graph, ctx, nil)
+  #appendToModule(module, finalNode)
+  moduleStmts.add finalNode
+  appendToModule(module, moduleStmts)
+
