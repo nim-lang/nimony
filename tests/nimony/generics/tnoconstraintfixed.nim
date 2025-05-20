32e5824d136e64b824237cffd09f4f24c63967ac
metagn <metagngn@gmail.com>
2025-05-20 20:38:21 +0300

    was it apparently

--- tests/nimony/generics/tnoconstraintfixed.nim
+++ tests/nimony/generics/tnoconstraintfixed.nim
@@ -9,9 +9,7 @@ proc `+=`[T: Addable](a: var T, b: T) =
 proc mean[T](vs: seq[T]): float {.untyped.} =
   result = 0
   for x in vs:
-    echo "got: ", x
     result += x
-  echo "final: ", result, " ", vs.len
   result / vs.len.float
 
 echo @[1.0, 2.0].mean
