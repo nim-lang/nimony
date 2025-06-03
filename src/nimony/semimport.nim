85a43676bf67e3b2d4b2fb48b43d5ad27cce58b3
Araq <rumpf_a@web.de>
2025-06-03 10:16:28 +0200

    progress

--- src/nimony/semimport.nim
+++ src/nimony/semimport.nim
@@ -51,7 +51,7 @@ proc importSingleFile(c: var SemContext; f1: ImportedFilename; origin: string;
   result = SymId(0)
   if not c.processedModules.contains(suffix):
     c.meta.importedFiles.add f2
-    if c.canSelfExec and needsRecompile(f2, suffixToNif suffix):
+    if (c.canSelfExec or c.inWhen > 0) and needsRecompile(f2, suffixToNif suffix):
       selfExec c, f2, (if f1.isSystem: " --isSystem" else: "")
 
     let moduleName = pool.strings.getOrIncl(f1.name)
