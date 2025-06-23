7d93b1e21f84abfd38030ce7cfd225b51b61d70f
ringabout <43030857+ringabout@users.noreply.github.com>
2025-06-23 20:57:17 +0800

    fixes #1203; if expressions contains return causes error in Hexer (#1220)
    fixes #1203
    
    follows
    https://github.com/nim-lang/nimony/pull/1210#discussion_r2160216864

--- tests/nimony/sysbasics/tconditionals.nim
+++ tests/nimony/sysbasics/tconditionals.nim
@@ -1,3 +1,5 @@
+import std/assertions
+
 proc foo = discard
 
 var x = if true:
@@ -5,3 +7,26 @@ var x = if true:
   else:
     foo()
     2
+
+block:
+  proc foo2: int =
+    var x = if true:
+        1
+      else:
+        return 2
+
+    result = x
+
+  assert foo2() == 1
+
+block:
+  proc foo2 =
+    var x = if true:
+        1
+      else:
+        var m = 12
+        return
+
+    assert x == 1
+
+  foo2()
