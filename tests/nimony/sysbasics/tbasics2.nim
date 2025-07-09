06ca05b350aedd12e048a19f88397dd199e9994b
ringabout <43030857+ringabout@users.noreply.github.com>
2025-07-09 20:24:01 +0800

    fixes lifting case objects fields with `var T destructor`

--- tests/nimony/sysbasics/tbasics2.nim
+++ tests/nimony/sysbasics/tbasics2.nim
@@ -86,3 +86,24 @@ block:
     let res = abend
 
   foo()
+
+
+type
+  E = enum
+    a, b, c, d
+  X = object
+    v: int
+  O = object
+    case kind: E
+    of a:
+      a: int
+    of b, c:
+      b: float
+    else:
+      d: X
+
+proc `=destroy`(x: var X) =
+  echo "x destroyed"
+
+var o = O(kind: d, d: X(v: 12345))
+assert o.d.v == 12345
