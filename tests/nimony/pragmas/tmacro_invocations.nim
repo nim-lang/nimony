7be698ff812f536a9533937b11c433c52b2d1a42
demotomohiro <gpuppur@gmail.com>
2025-07-13 17:36:13 +0900

    can call multiple templates

--- tests/nimony/pragmas/tmacro_invocations.nim
+++ tests/nimony/pragmas/tmacro_invocations.nim
@@ -4,4 +4,10 @@ template testTmpl1(procdef: untyped) {.untyped.} =
   let s = astToStr(procdef)
   echo "`" & s & "`"
 
+template testTmplDup(procdef: untyped) {.untyped.} =
+  procdef
+  procdef
+
 proc foo1() {.testTmpl1.} = discard
+proc foo2() {.testTmplDup, testTmpl1.} = discard
+proc foo3() {.testTmplDup, testTmpl1, testTmplDup.} = discard
