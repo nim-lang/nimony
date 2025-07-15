fbf7610e292d6c98e8946a19ae498c6efe7bdadd
Araq <rumpf_a@web.de>
2025-07-15 12:04:16 +0200

    make destroy calls avoid the virtual call overhead if possible

--- src/hexer/duplifier.nim
+++ src/hexer/duplifier.nim
@@ -270,8 +270,11 @@ proc evalLeftHandSide(c: var Context; le: var Cursor): TokenBuf =
       copyIntoSymUse result, tmp, info
     c.typeCache.registerLocalPtrOf(tmp, VarY, typ)
 
-proc callDestroy(c: var Context; destroyProc: SymId; arg: TokenBuf) =
+proc callDestroy(c: var Context; destroyProc: SymId; arg: TokenBuf; typ: Cursor) =
   let info = arg[0].info
+  let staticCall = typ.typeKind notin {RefT, PtrT}
+  if staticCall:
+    c.dest.addParLe ProcCallX, info
   copyIntoKind c.dest, CallS, info:
     copyIntoSymUse c.dest, destroyProc, info
     if isMutFirstParam(destroyProc):
@@ -279,11 +282,18 @@ proc callDestroy(c: var Context; destroyProc: SymId; arg: TokenBuf) =
         copyTree c.dest, arg
     else:
       copyTree c.dest, arg
+  if staticCall:
+    c.dest.addParRi()
 
-proc callDestroy(c: var Context; destroyProc: SymId; arg: SymId; info: PackedLineInfo) =
+proc callDestroy(c: var Context; destroyProc: SymId; arg: SymId; info: PackedLineInfo; typ: Cursor) =
+  let staticCall = typ.typeKind notin {RefT, PtrT}
+  if staticCall:
+    c.dest.addParLe ProcCallX, info
   copyIntoKind c.dest, CallS, info:
     copyIntoSymUse c.dest, destroyProc, info
     copyIntoSymUse c.dest, arg, info
+  if staticCall:
+    c.dest.addParRi()
 
 proc tempOfTrArg(c: var Context; n: Cursor; typ: Cursor): SymId =
   var n = n
@@ -381,7 +391,7 @@ proc trAsgn(c: var Context; n: var Cursor) =
       if not potentialAliasing(le, ri):
         # `x = f()` is turned into `=destroy(x); x =bitcopy f()`.
         if isNotFirstAsgn:
-          callDestroy(c, destructor, lhs)
+          callDestroy(c, destructor, lhs, leType)
         copyInto c.dest, n:
           copyTree c.dest, lhs
           n = ri
@@ -390,7 +400,7 @@ proc trAsgn(c: var Context; n: var Cursor) =
         # `x = f()` is turned into `let tmp = f(); =destroy(x); x =bitcopy tmp`.
         let tmp = tempOfTrArg(c, ri, leType)
         if isNotFirstAsgn:
-          callDestroy(c, destructor, lhs)
+          callDestroy(c, destructor, lhs, leType)
         copyInto c.dest, n:
           copyTree c.dest, lhs
           copyIntoSymUse c.dest, tmp, ri.info
@@ -401,7 +411,7 @@ proc trAsgn(c: var Context; n: var Cursor) =
         # `let tmp = y; =wasMoved(y); =destroy(x); x =bitcopy tmp`
         let tmp = tempOfTrArg(c, ri, leType)
         callWasMoved c, ri, leType
-        callDestroy(c, destructor, lhs)
+        callDestroy(c, destructor, lhs, leType)
         copyInto c.dest, n:
           var lhsAsCursor = cursorAt(lhs, 0)
           tr c, lhsAsCursor, DontCare
@@ -410,7 +420,7 @@ proc trAsgn(c: var Context; n: var Cursor) =
           skip n # skip right hand side
       else:
         if isNotFirstAsgn:
-          callDestroy(c, destructor, lhs)
+          callDestroy(c, destructor, lhs, leType)
         copyInto c.dest, n:
           copyTree c.dest, lhs
           n = ri
@@ -426,10 +436,10 @@ proc trAsgn(c: var Context; n: var Cursor) =
           tr c, lhsAsCursor, DontCare
           n = ri
           callDup c, n
-        callDestroy(c, destructor, tmp, le.info)
+        callDestroy(c, destructor, tmp, le.info, leType)
       else:
         if isNotFirstAsgn:
-          callDestroy(c, destructor, lhs)
+          callDestroy(c, destructor, lhs, leType)
         copyInto c.dest, n:
           var lhsAsCursor = cursorAt(lhs, 0)
           tr c, lhsAsCursor, DontCare
