3e417388a47b3cd06647798e916316667029ccff
demotomohiro <gpuppur@gmail.com>
2025-08-04 13:10:42 +0900

    Suppress the warning generated when passing const sets to procedures

--- src/nifc/cprelude.nim
+++ src/nifc/cprelude.nim
@@ -170,6 +170,7 @@ typedef NU8 NU;
 #  pragma GCC diagnostic ignored "-Wswitch-bool"
 #  pragma GCC diagnostic ignored "-Wformat"
 #  pragma GCC diagnostic ignored "-Wpointer-sign"
+#  pragma GCC diagnostic ignored "-Wdiscarded-qualifiers"
 #endif
 
 
