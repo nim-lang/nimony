012f96b8e7683dd7a9c8e61fd83db0afb1fce53b
Tomohiro <gpuppur@gmail.com>
2025-08-05 14:05:23 +0900

    ports strutils.nim in Nim 2 to Nimony (#1354)

--- lib/std/strutils.nim
+++ lib/std/strutils.nim
@@ -3,6 +3,129 @@ const
     ## All the characters that count as whitespace (space, tab, vertical tab,
     ## carriage return, new line, form feed).
 
+  Letters* = {'A'..'Z', 'a'..'z'}
+    ## The set of letters.
+
+  UppercaseLetters* = {'A'..'Z'}
+    ## The set of uppercase ASCII letters.
+
+  LowercaseLetters* = {'a'..'z'}
+    ## The set of lowercase ASCII letters.
+
+  PunctuationChars* = {'!'..'/', ':'..'@', '['..'`', '{'..'~'}
+    ## The set of all ASCII punctuation characters.
+
+  Digits* = {'0'..'9'}
+    ## The set of digits.
+
+  HexDigits* = {'0'..'9', 'A'..'F', 'a'..'f'}
+    ## The set of hexadecimal digits.
+
+  IdentChars* = {'a'..'z', 'A'..'Z', '0'..'9', '_'}
+    ## The set of characters an identifier can consist of.
+
+  IdentStartChars* = {'a'..'z', 'A'..'Z', '_'}
+    ## The set of characters an identifier can start with.
+
+  Newlines* = {'\13', '\10'}
+    ## The set of characters a newline terminator can start with (carriage
+    ## return, line feed).
+
+  PrintableChars* = Letters + Digits + PunctuationChars + Whitespace
+    ## The set of all printable ASCII characters (letters, digits, whitespace, and punctuation characters).
+
+  AllChars* = {'\x00'..'\xFF'}
+    ## A set with all the possible characters.
+    ##
+    ## Not very useful by its own, you can use it to create *inverted* sets to
+    ## make the `find func<#find,string,set[char],Natural,int>`_
+    ## find **invalid** characters in strings. Example:
+    ##   ```nim
+    ##   let invalid = AllChars - Digits
+    ##   doAssert "01234".find(invalid) == -1
+    ##   doAssert "01A34".find(invalid) == 2
+    ##   ```
+
+func isAlphaAscii*(c: char): bool {.inline.} =
+  ## Checks whether or not character `c` is alphabetical.
+  ##
+  ## This checks a-z, A-Z ASCII characters only.
+  ## Use `Unicode module<unicode.html>`_ for UTF-8 support.
+  runnableExamples:
+    assert isAlphaAscii('e') == true
+    assert isAlphaAscii('E') == true
+    assert isAlphaAscii('8') == false
+  c in Letters
+
+func isAlphaNumeric*(c: char): bool {.inline.} =
+  ## Checks whether or not `c` is alphanumeric.
+  ##
+  ## This checks a-z, A-Z, 0-9 ASCII characters only.
+  runnableExamples:
+    assert isAlphaNumeric('n') == true
+    assert isAlphaNumeric('8') == true
+    assert isAlphaNumeric(' ') == false
+  c in Letters+Digits
+
+func isDigit*(c: char): bool {.inline.} =
+  ## Checks whether or not `c` is a number.
+  ##
+  ## This checks 0-9 ASCII characters only.
+  runnableExamples:
+    assert isDigit('n') == false
+    assert isDigit('8') == true
+  c in Digits
+
+func isSpaceAscii*(c: char): bool {.inline.} =
+  ## Checks whether or not `c` is a whitespace character.
+  runnableExamples:
+    assert isSpaceAscii('n') == false
+    assert isSpaceAscii(' ') == true
+    assert isSpaceAscii('\t') == true
+  c in Whitespace
+
+func isLowerAscii*(c: char): bool {.inline.} =
+  ## Checks whether or not `c` is a lower case character.
+  ##
+  ## This checks ASCII characters only.
+  ## Use `Unicode module<unicode.html>`_ for UTF-8 support.
+  ##
+  ## See also:
+  ## * `toLowerAscii func<#toLowerAscii,char>`_
+  runnableExamples:
+    assert isLowerAscii('e') == true
+    assert isLowerAscii('E') == false
+    assert isLowerAscii('7') == false
+  c in LowercaseLetters
+
+func isUpperAscii*(c: char): bool {.inline.} =
+  ## Checks whether or not `c` is an upper case character.
+  ##
+  ## This checks ASCII characters only.
+  ## Use `Unicode module<unicode.html>`_ for UTF-8 support.
+  ##
+  ## See also:
+  ## * `toUpperAscii func<#toUpperAscii,char>`_
+  runnableExamples:
+    assert isUpperAscii('e') == false
+    assert isUpperAscii('E') == true
+    assert isUpperAscii('7') == false
+  c in UppercaseLetters
+
+func allCharsInSet*(s: string; theSet: set[char]): bool =
+  ## Returns true if every character of `s` is in the set `theSet`.
+  runnableExamples:
+    assert allCharsInSet("aeea", {'a', 'e'}) == true
+    assert allCharsInSet("", {'a', 'e'}) == true
+
+  for c in items(s):
+    if c notin theSet: return false
+  return true
+
+func isEmptyOrWhitespace*(s: string): bool {.inline.} =
+  ## Checks if `s` is empty or consists entirely of whitespace characters.
+  result = s.allCharsInSet(Whitespace)
+
 proc endsWith*(s: string; c: char): bool {.inline.} =
   if s.len > 0: s[s.len-1] == c else: false
 
@@ -19,6 +142,15 @@ proc `$`*(x: char): string =
   result[0] = x
 
 func continuesWith*(s, prefix: string; start: int): bool =
+  ## Returns true if `s` continues with `prefix` at position `start`.
+  ##
+  ## If `prefix == ""` true is returned.
+  ##
+  ## See also:
+  ## * `startsWith func<#startsWith,string,string>`_
+  ## * `endsWith func<#endsWith,string,string>`_
+  if prefix.len == 0:
+    return true
   if prefix.len > s.len-start:
     return false
   for i in 0 ..< prefix.len:
@@ -27,11 +159,172 @@ func continuesWith*(s, prefix: string; start: int): bool =
   return true
 
 func startsWith*(s, prefix: string): bool =
+  ## Returns true if `s` starts with string `prefix`.
+  ##
+  ## If `prefix == ""` true is returned.
+  ##
+  ## See also:
+  ## * `endsWith func<#endsWith,string,string>`_
+  ## * `continuesWith func<#continuesWith,string,string,Natural>`_
+  ## * `removePrefix func<#removePrefix,string,string>`_
+  runnableExamples:
+    let a = "abracadabra"
+    assert a.startsWith("abra") == true
+    assert a.startsWith("bra") == false
   continuesWith s, prefix, 0
 
+func endsWith*(s, suffix: string): bool =
+  ## Returns true if `s` ends with `suffix`.
+  ##
+  ## If `suffix == ""` true is returned.
+  ##
+  ## See also:
+  ## * `startsWith func<#startsWith,string,string>`_
+  ## * `continuesWith func<#continuesWith,string,string,Natural>`_
+  ## * `removeSuffix func<#removeSuffix,string,string>`_
+  runnableExamples:
+    let a = "abracadabra"
+    assert a.endsWith("abra") == true
+    assert a.endsWith("dab") == false
+  if suffix.len > s.len:
+    return false
+  continuesWith s, suffix, s.len - suffix.len
+
 proc toLowerAscii*(c: char): char {.inline.} =
+  ## Returns the lower case version of character `c`.
+  ##
+  ## This works only for the letters `A-Z`. See `unicode.toLower
+  ## <unicode.html#toLower,Rune>`_ for a version that works for any Unicode
+  ## character.
+  ##
+  ## See also:
+  ## * `isLowerAscii func<#isLowerAscii,char>`_
+  ## * `toLowerAscii func<#toLowerAscii,string>`_ for converting a string
+  runnableExamples:
+    assert toLowerAscii('A') == 'a'
+    assert toLowerAscii('e') == 'e'
   if c >= 'A' and c <= 'Z': char(int(c) - int('A') + int('a'))
   else: c
+
+func toLowerAscii*(s: string): string =
+  ## Converts string `s` into lower case.
+  ##
+  ## This works only for the letters `A-Z`. See `unicode.toLower
+  ## <unicode.html#toLower,string>`_ for a version that works for any Unicode
+  ## character.
+  ##
+  ## See also:
+  ## * `normalize func<#normalize,string>`_
+  runnableExamples:
+    assert toLowerAscii("FooBar!") == "foobar!"
+  result = newString(s.len)
+  for i in 0 ..< s.len:
+    result[i] = toLowerAscii(s[i])
+
+func toUpperAscii*(c: char): char {.inline.} =
+  ## Converts character `c` into upper case.
+  ##
+  ## This works only for the letters `A-Z`.  See `unicode.toUpper
+  ## <unicode.html#toUpper,Rune>`_ for a version that works for any Unicode
+  ## character.
+  ##
+  ## See also:
+  ## * `isUpperAscii func<#isUpperAscii,char>`_
+  ## * `toUpperAscii func<#toUpperAscii,string>`_ for converting a string
+  ## * `capitalizeAscii func<#capitalizeAscii,string>`_
+  runnableExamples:
+    assert toUpperAscii('a') == 'A'
+    assert toUpperAscii('E') == 'E'
+  if c >= 'a' and c <= 'z': char(int(c) - int('a') + int('A'))
+  else: c
+
+func toUpperAscii*(s: string): string =
+  ## Converts string `s` into upper case.
+  ##
+  ## This works only for the letters `A-Z`.  See `unicode.toUpper
+  ## <unicode.html#toUpper,string>`_ for a version that works for any Unicode
+  ## character.
+  ##
+  ## See also:
+  ## * `capitalizeAscii func<#capitalizeAscii,string>`_
+  runnableExamples:
+    assert toUpperAscii("FooBar!") == "FOOBAR!"
+  result = newString(s.len)
+  for i in 0 ..< s.len:
+    result[i] = toUpperAscii(s[i])
+
+func capitalizeAscii*(s: string): string {.inline.} =
+  ## Converts the first character of string `s` into upper case.
+  ##
+  ## This works only for the letters `A-Z`.
+  ## Use `Unicode module<unicode.html>`_ for UTF-8 support.
+  ##
+  ## See also:
+  ## * `toUpperAscii func<#toUpperAscii,char>`_
+  runnableExamples:
+    assert capitalizeAscii("foo") == "Foo"
+    assert capitalizeAscii("-bar") == "-bar"
+  if s.len == 0: result = ""
+  else:
+    result = s
+    result[0] = toUpperAscii(result[0])
+
+func cmpIgnoreCase*(a, b: string): int =
+  ## Compares two strings in a case insensitive manner. Returns:
+  ##
+  ## | `0` if a == b
+  ## | `< 0` if a < b
+  ## | `> 0` if a > b
+  runnableExamples:
+    assert cmpIgnoreCase("FooBar", "foobar") == 0
+    assert cmpIgnoreCase("bar", "Foo") < 0
+    assert cmpIgnoreCase("Foo5", "foo4") > 0
+
+  let aLen = a.len
+  let bLen = b.len
+  let minLen = min(aLen, bLen)
+  for i in 0 ..< minLen:
+    result = a[i].toLowerAscii.ord - b[i].toLowerAscii.ord
+    if result != 0: return
+  result = aLen - bLen
+
+func cmpIgnoreStyle*(a, b: string): int =
+  ## Semantically the same as `cmp(normalize(a), normalize(b))`. It
+  ## is just optimized to not allocate temporary strings. This should
+  ## NOT be used to compare Nim identifier names.
+  ## Use `macros.eqIdent<macros.html#eqIdent,string,string>`_ for that.
+  ##
+  ## Returns:
+  ##
+  ## | `0` if a == b
+  ## | `< 0` if a < b
+  ## | `> 0` if a > b
+  runnableExamples:
+    assert cmpIgnoreStyle("foo_bar", "FooBar") == 0
+    assert cmpIgnoreStyle("foo_bar_5", "FooBar4") > 0
+  let aLen = a.len
+  let bLen = b.len
+  var i = 0
+  var j = 0
+  while true:
+    while i < aLen and a[i] == '_': inc i
+    while j < bLen and b[j] == '_': inc j
+    if i == aLen:
+      if j == bLen:
+        # both cursors at the end:
+        return 0
+      else:
+        # not yet at the end of 'b':
+        return -1
+    elif j == bLen:
+      return 1
+    let aa = toLowerAscii(a[i])
+    let bb = toLowerAscii(b[j])
+    result = ord(aa) - ord(bb)
+    if result != 0: return result
+    # the characters are identical:
+    inc i
+    inc j
 
 func replace*(s: string; sub, by: char): string =
   result = newString(s.len)
---
+++ tests/nimony/stdlib/tstrutils.nim
@@ -0,0 +1,220 @@
+import std/[assertions, strutils]
+
+block: # isAlphaAscii
+  assert isAlphaAscii('r')
+  assert isAlphaAscii('A')
+  assert not isAlphaAscii('$')
+
+block: # isAlphaNumeric
+  assert isAlphaNumeric('3')
+  assert isAlphaNumeric('R')
+  assert not isAlphaNumeric('!')
+
+block: # isDigit
+  assert isDigit('3')
+  assert not isDigit('a')
+  assert not isDigit('%')
+
+block: # isSpaceAscii
+  assert isSpaceAscii('\t')
+  assert isSpaceAscii('\l')
+  assert not isSpaceAscii('A')
+
+block: # isLowerAscii
+  assert isLowerAscii('a')
+  assert isLowerAscii('z')
+  assert not isLowerAscii('A')
+  assert not isLowerAscii('5')
+  assert not isLowerAscii('&')
+  assert not isLowerAscii(' ')
+
+block: # isUpperAscii
+  assert isUpperAscii('A')
+  assert not isUpperAscii('b')
+  assert not isUpperAscii('5')
+  assert not isUpperAscii('%')
+
+block: # allCharsInSet
+  assert allCharsInSet("abc", {'a'..'z'})
+  assert not allCharsInSet("123", {'a'..'z'})
+  assert not allCharsInSet("abc123", {'a'..'z'})
+  assert allCharsInSet("123abc", {'a'..'z', '0'..'9'})
+  assert not allCharsInSet("ABC _", {'a'..'z', '0'..'9'})
+  assert allCharsInSet("", {})
+
+block: # isEmptyOrWhitespace
+  assert isEmptyOrWhitespace("")
+  assert isEmptyOrWhitespace("       ")
+  assert isEmptyOrWhitespace("\t\l \v\r\f")
+  assert not isEmptyOrWhitespace("ABc   \td")
+  assert not isEmptyOrWhitespace("a")
+
+block: # continuesWith
+  assert continuesWith("", "", 0)
+  assert continuesWith("", "", 1)
+  assert not continuesWith("", "a", 0)
+  assert not continuesWith("", "a", 1)
+  assert continuesWith("a", "", 0)
+  assert continuesWith("a", "", 1)
+  assert continuesWith("a", "a", 0)
+  assert not continuesWith("a", "aa", 0)
+  assert continuesWith("aa", "a", 0)
+  assert continuesWith("aa", "a", 1)
+  assert not continuesWith("aa", "a", 2)
+  assert not continuesWith("aa", "a", 3)
+  assert continuesWith("ab", "a", 0)
+  assert not continuesWith("ab", "a", 1)
+  assert not continuesWith("ba", "a", 0)
+  assert continuesWith("ba", "a", 1)
+  assert continuesWith("bab", "a", 1)
+  assert not continuesWith("bab", "a", 2)
+  assert not continuesWith("bab", "a", 3)
+  assert not continuesWith("a", "ab", 0)
+  assert continuesWith("ab", "ab", 0)
+  assert not continuesWith("AB", "ab", 0)
+  assert not continuesWith("ab", "ab", 1)
+  assert not continuesWith("ab", "ab", 2)
+  assert continuesWith("abc", "ab", 0)
+  assert not continuesWith("abc", "ab", 1)
+  assert not continuesWith("abc", "bc", 0)
+  assert continuesWith("abc", "bc", 1)
+  assert not continuesWith("abc", "bc", 2)
+
+block: # startsWith
+  assert startsWith("", "")
+  assert not startsWith("", "a")
+  assert startsWith("a", "")
+  assert startsWith("a", "a")
+  assert not startsWith("a", "A")
+  assert startsWith("aa", "a")
+  assert startsWith("ab", "a")
+  assert not startsWith("ba", "a")
+  assert startsWith("ab", "ab")
+  assert startsWith("abc", "ab")
+  assert not startsWith("abc", "bc")
+  assert startsWith("abc", "abc")
+  assert not startsWith("bc", "abc")
+  assert startsWith("abcd", "abc")
+
+block: # endsWith
+  assert endsWith("", "")
+  assert not endsWith("", "a")
+  assert endsWith("a", "")
+  assert endsWith("a", "a")
+  assert not endsWith("a", "A")
+  assert endsWith("aa", "a")
+  assert not endsWith("ab", "a")
+  assert endsWith("ba", "a")
+  assert endsWith("ab", "ab")
+  assert not endsWith("abc", "ab")
+  assert endsWith("abc", "bc")
+  assert endsWith("abc", "abc")
+  assert not endsWith("bc", "abc")
+  assert not endsWith("abcd", "abc")
+  assert endsWith("abcd", "bcd")
+
+block: # toLowerAscii
+  assert toLowerAscii('A') == 'a'
+  assert toLowerAscii('B') == 'b'
+  assert toLowerAscii('Y') == 'y'
+  assert toLowerAscii('Z') == 'z'
+  assert toLowerAscii('a') == 'a'
+  assert toLowerAscii('b') == 'b'
+  assert toLowerAscii(' ') == ' '
+  assert toLowerAscii('1') == '1'
+  assert toLowerAscii('(') == '('
+
+block: # toLowerAscii*(s: string): string
+  assert toLowerAscii("FOO") == "foo"
+  assert toLowerAscii("fOO") == "foo"
+  assert toLowerAscii("foo") == "foo"
+  assert toLowerAscii(" bAr123_") == " bar123_"
+  assert toLowerAscii(" bar123_") == " bar123_"
+
+block: # toUpperAscii
+  assert toUpperAscii('a') == 'A'
+  assert toUpperAscii('b') == 'B'
+  assert toUpperAscii('y') == 'Y'
+  assert toUpperAscii('z') == 'Z'
+  assert toUpperAscii('A') == 'A'
+  assert toUpperAscii('B') == 'B'
+  assert toUpperAscii(' ') == ' '
+  assert toUpperAscii('1') == '1'
+  assert toUpperAscii('(') == '('
+
+block: # toUpperAscii*(s: string): string
+  assert toUpperAscii("foo") == "FOO"
+  assert toUpperAscii("Foo") == "FOO"
+  assert toUpperAscii("FOO") == "FOO"
+  assert toUpperAscii(" BaR123_") == " BAR123_"
+  assert toUpperAscii(" BAR123_") == " BAR123_"
+
+block: # capitalizeAscii
+  assert capitalizeAscii("foo") == "Foo"
+  assert capitalizeAscii("1bar") == "1bar"
+
+block: # cmpIgnoreCase
+  assert cmpIgnoreCase("", "") == 0
+  assert cmpIgnoreCase("", "a") < 0
+  assert cmpIgnoreCase("a", "") > 0
+  assert cmpIgnoreCase("a", "a") == 0
+  assert cmpIgnoreCase("a", "A") == 0
+  assert cmpIgnoreCase("A", "a") == 0
+  assert cmpIgnoreCase("A", "A") == 0
+  assert cmpIgnoreCase("a", "b") < 0
+  assert cmpIgnoreCase("b", "a") > 0
+  assert cmpIgnoreCase("a", "aa") < 0
+  assert cmpIgnoreCase("aa", "a") > 0
+  assert cmpIgnoreCase("ab", "ac") < 0
+  assert cmpIgnoreCase("ab", "aC") < 0
+  assert cmpIgnoreCase("aB", "ac") < 0
+  assert cmpIgnoreCase("aB", "aC") < 0
+  assert cmpIgnoreCase("aac", "aab") > 0
+  assert cmpIgnoreCase("aaC", "aab") > 0
+  assert cmpIgnoreCase("aac", "aaB") > 0
+  assert cmpIgnoreCase("aaC", "aaB") > 0
+  assert cmpIgnoreCase("foobar", "foobar") == 0
+  assert cmpIgnoreCase("foobar", "Foobar") == 0
+  assert cmpIgnoreCase("fooBar", "foobar") == 0
+  assert cmpIgnoreCase("FOOBAR", "foobar") == 0
+  assert cmpIgnoreCase("FOOBAR", "foobar1") < 0
+  assert cmpIgnoreCase("foobar", "FOOBAR1") < 0
+
+block: # cmpIgnoreStyle
+  assert cmpIgnoreStyle("", "") == 0
+  assert cmpIgnoreStyle("", "_") == 0
+  assert cmpIgnoreStyle("_", "") == 0
+  assert cmpIgnoreStyle("_", "_") == 0
+  assert cmpIgnoreStyle("_", "__") == 0
+  assert cmpIgnoreStyle("__", "_") == 0
+  assert cmpIgnoreStyle("__", "__") == 0
+  assert cmpIgnoreStyle("", "a") < 0
+  assert cmpIgnoreStyle("a", "") > 0
+  assert cmpIgnoreStyle("a", "a") == 0
+  assert cmpIgnoreStyle("", "_a") < 0
+  assert cmpIgnoreStyle("_a", "") > 0
+  assert cmpIgnoreStyle("_", "_a") < 0
+  assert cmpIgnoreStyle("_a", "_") > 0
+  assert cmpIgnoreStyle("a", "b") < 0
+  assert cmpIgnoreStyle("b", "a") > 0
+  assert cmpIgnoreStyle("A", "b") < 0
+  assert cmpIgnoreStyle("B", "a") > 0
+  assert cmpIgnoreStyle("B", "a") > 0
+  assert cmpIgnoreStyle("aa", "aa") == 0
+  assert cmpIgnoreStyle("aa", "aaa") < 0
+  assert cmpIgnoreStyle("aaa", "aa") > 0
+  assert cmpIgnoreStyle("aA", "aaa") < 0
+  assert cmpIgnoreStyle("aAa", "aA") > 0
+  assert cmpIgnoreStyle("a", "_a") == 0
+  assert cmpIgnoreStyle("_a", "a") == 0
+  assert cmpIgnoreStyle("_a", "_a") == 0
+  assert cmpIgnoreStyle("a", "_aa") < 0
+  assert cmpIgnoreStyle("_a", "aa") < 0
+  assert cmpIgnoreStyle("_a", "_aa") < 0
+  assert cmpIgnoreStyle("a", "_a__a__") < 0
+  assert cmpIgnoreStyle("a_b", "_ab_") == 0
+  assert cmpIgnoreStyle("a_b_c", "_ab__c") == 0
+  assert cmpIgnoreStyle("abc", "_a__b__c_") == 0
+  assert cmpIgnoreStyle("a_B", "_ab_") == 0
+  assert cmpIgnoreStyle("a_b_C", "_ab__c") == 0
+  assert cmpIgnoreStyle("ABC", "_a__b__c_") == 0
