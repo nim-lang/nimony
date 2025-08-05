27473998cd4ada75adca3909305ca9936fc166bd
ringabout <43030857+ringabout@users.noreply.github.com>
2025-08-05 22:56:56 +0800

    dynlib is yet implemented

--- tests/nimony/sysbasics/tlocks.nim
+++ tests/nimony/sysbasics/tlocks.nim
@@ -1,42 +1,44 @@
-import std/locks
-import std/assertions
+when not defined(windows):
+  # TODO: remove this when dynlib is implemented
+  import std/locks
+  import std/assertions
 
 
-proc testInitAndDeinitLock() =
-  var lock: Lock = default(Lock)
-  initLock(lock)
-  deinitLock(lock)
+  proc testInitAndDeinitLock() =
+    var lock: Lock = default(Lock)
+    initLock(lock)
+    deinitLock(lock)
 
-proc testTryAcquireAndRelease() =
-  var lock: Lock = default(Lock)
-  initLock(lock)
-  let acquired = tryAcquire(lock)
-  if acquired:
+  proc testTryAcquireAndRelease() =
+    var lock: Lock = default(Lock)
+    initLock(lock)
+    let acquired = tryAcquire(lock)
+    if acquired:
+      release(lock)
+    deinitLock(lock)
+
+  proc testAcquireAndRelease() =
+    var lock: Lock = default(Lock)
+    initLock(lock)
+    acquire(lock)
     release(lock)
-  deinitLock(lock)
+    deinitLock(lock)
 
-proc testAcquireAndRelease() =
-  var lock: Lock = default(Lock)
-  initLock(lock)
-  acquire(lock)
-  release(lock)
-  deinitLock(lock)
+  proc testInitAndDeinitCond() =
+    var cond: Cond = default(Cond)
+    initCond(cond)
+    deinitCond(cond)
 
-proc testInitAndDeinitCond() =
-  var cond: Cond = default(Cond)
-  initCond(cond)
-  deinitCond(cond)
-
-proc testSignalAndBroadcast() =
-  var cond: Cond = default(Cond)
-  initCond(cond)
-  signal(cond)
-  broadcast(cond)
-  deinitCond(cond)
+  proc testSignalAndBroadcast() =
+    var cond: Cond = default(Cond)
+    initCond(cond)
+    signal(cond)
+    broadcast(cond)
+    deinitCond(cond)
 
 
-testInitAndDeinitLock()
-testTryAcquireAndRelease()
-testAcquireAndRelease()
-testInitAndDeinitCond()
-testSignalAndBroadcast()
+  testInitAndDeinitLock()
+  testTryAcquireAndRelease()
+  testAcquireAndRelease()
+  testInitAndDeinitCond()
+  testSignalAndBroadcast()
