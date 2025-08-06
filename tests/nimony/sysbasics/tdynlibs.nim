d4a3b58326b2728bcad92f8e98b518d83cd065f7
ringabout <43030857+ringabout@users.noreply.github.com>
2025-08-06 22:20:37 +0800

    progress: tests

--- tests/nimony/sysbasics/tdynlibs.nim
+++ tests/nimony/sysbasics/tdynlibs.nim
@@ -24,3 +24,17 @@ when defined(posix):
 
   iconvClose(s)
   iconvClose(s2)
+
+  proc fox() =
+    let s = iconvOpen("12", "12")
+    let s2 = iconvOpen("12", "13")
+    iconvClose(s)
+    iconvClose(s2)
+
+  fox()
+
+  block:
+    let s = iconvOpen("12", "12")
+    let s2 = iconvOpen("12", "13")
+    iconvClose(s)
+    iconvClose(s2)
