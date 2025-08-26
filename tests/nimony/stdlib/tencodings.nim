765eea48495a080b0d256046ae5182523ea56a71
ringabout <43030857+ringabout@users.noreply.github.com>
2025-08-26 22:37:30 +0800

    FIXES WINDOWS

--- tests/nimony/stdlib/tencodings.nim
+++ tests/nimony/stdlib/tencodings.nim
@@ -55,49 +55,47 @@ when defined(windows):
     let original = "\x42\x04\x35\x04\x41\x04\x42\x04" # utf-16 little endian test string "тест"
     noRaise:
       let result = convert(original, "utf-8", "utf-16")
-    assert(result == "\xd1\x82\xd0\xb5\xd1\x81\xd1\x82")
+      assert(result == "\xd1\x82\xd0\xb5\xd1\x81\xd1\x82")
 
   block should_convert_from_utf16_to_win1251:
     let original = "\x42\x04\x35\x04\x41\x04\x42\x04" # utf-16 little endian test string "тест"
     noRaise:
       let result = convert(original, "windows-1251", "utf-16")
-    assert(result == "\xf2\xe5\xf1\xf2")
+      assert(result == "\xf2\xe5\xf1\xf2")
 
   block should_convert_from_win1251_to_koi8r:
     let original = "\xf2\xe5\xf1\xf2" # win1251 test string "тест"
     noRaise:
       let result = convert(original, "koi8-r", "windows-1251")
-    assert(result == "\xd4\xc5\xd3\xd4")
+      assert(result == "\xd4\xc5\xd3\xd4")
 
   block should_convert_from_koi8r_to_win1251:
     let original = "\xd4\xc5\xd3\xd4" # koi8r test string "тест"
     noRaise:
       let result = convert(original, "windows-1251", "koi8-r")
-    assert(result == "\xf2\xe5\xf1\xf2")
+      assert(result == "\xf2\xe5\xf1\xf2")
 
   block should_convert_from_utf8_to_win1251:
     let original = "\xd1\x82\xd0\xb5\xd1\x81\xd1\x82" # utf-8 test string "тест"
     noRaise:
       let result = convert(original, "windows-1251", "utf-8")
-    assert(result == "\xf2\xe5\xf1\xf2")
+      assert(result == "\xf2\xe5\xf1\xf2")
 
   block should_convert_from_utf8_to_utf16:
     let original = "\xd1\x82\xd0\xb5\xd1\x81\xd1\x82" # utf-8 test string "тест"
     noRaise:
       let result = convert(original, "utf-16", "utf-8")
-    assert(result == "\x42\x04\x35\x04\x41\x04\x42\x04")
+      assert(result == "\x42\x04\x35\x04\x41\x04\x42\x04")
 
   block should_handle_empty_string_for_any_conversion:
     let original = ""
     noRaise:
       var result = convert(original, "utf-16", "utf-8")
-    assert(result == "")
-    noRaise:
+      assert(result == "")
       result = convert(original, "utf-8", "utf-16")
-    assert(result == "")
-    noRaise:
+      assert(result == "")
       result = convert(original, "windows-1251", "koi8-r")
-    assert(result == "")
+      assert(result == "")
 
 block:
   var cp1252: string
@@ -105,7 +103,6 @@ block:
   let orig = "öäüß"
   noRaise:
     cp1252 = convert(orig, "CP1252", "UTF-8")
-  noRaise:
     ibm850 = convert(cp1252, "ibm850", "CP1252")
   let current = getCurrentEncoding()
   assert orig == "\195\182\195\164\195\188\195\159"
