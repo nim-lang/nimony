386255ad8e814e77a5382b20e875f81ac1dd4a3e
ringabout <43030857+ringabout@users.noreply.github.com>
2025-08-26 21:17:59 +0800

    fixes memfiles

--- tests/nimony/stdlib/tmemfiles.nim
+++ tests/nimony/stdlib/tmemfiles.nim
@@ -1,7 +1,10 @@
 import std/[assertions, memfiles]
 
 block:
-  var f = memfiles.open("tests/nimony/stdlib/file_for_reading_test.txt")
-  let s = cast[cstring](f.mem).borrowCStringUnsafe(f.size)
-  assert s == "Test text\n"
-  f.close
+  try:
+    var f = memfiles.open("tests/nimony/stdlib/file_for_reading_test.txt")
+    let s = cast[cstring](f.mem).borrowCStringUnsafe(f.size)
+    assert s == "Test text\n"
+    f.close
+  except:
+    assert false
