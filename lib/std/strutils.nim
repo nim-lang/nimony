3cb06e450d20f4acd7f5e25eab8b519207cdbbc1
demotomohiro <gpuppur@gmail.com>
2025-08-30 05:22:44 +0900

    adds replace

--- lib/std/strutils.nim
+++ lib/std/strutils.nim
@@ -472,6 +472,47 @@ func replace*(s: string; sub, by: char): string =
     else: result[i] = s[i]
     inc i
 
+func replace*(s, sub: string; by = ""): string =
+  ## Replaces every occurrence of the string `sub` in `s` with the string `by`.
+  ##
+  ## See also:
+  ## * `find func<#find,string,string,Natural,int>`_
+  ## * `replace func<#replace,string,char,char>`_ for replacing
+  ##   single characters
+  ## * `replaceWord func<#replaceWord,string,string,string>`_
+  ## * `multiReplace func<#multiReplace,string,varargs[]>`_ for substrings
+  ## * `multiReplace func<#multiReplace,openArray[char],varargs[]>`_ for single characters
+  result = ""
+  let subLen = sub.len
+  if subLen == 0:
+    result = s
+  elif subLen == 1:
+    # when the pattern is a single char, we use a faster
+    # char-based search that doesn't need a skip table:
+    let c = sub[0]
+    let last = s.high
+    var i = 0
+    while true:
+      let j = find(s, c, i, last)
+      if j < 0: break
+      add result, substr(s, i, j - 1)
+      add result, by
+      i = j + subLen
+    # copy the rest:
+    add result, substr(s, i)
+  else:
+    var a = initSkipTable(sub)
+    let last = s.high
+    var i = 0
+    while true:
+      let j = find(a, s, sub, i, last)
+      if j < 0: break
+      add result, substr(s, i, j - 1)
+      add result, by
+      i = j + subLen
+    # copy the rest:
+    add result, substr(s, i)
+
 const HexChars = "0123456789ABCDEF"
 
 func escape*(s: string, prefix = "\"", suffix = "\""): string =
