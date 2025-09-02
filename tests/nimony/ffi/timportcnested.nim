964ef3b9653d9e911455f0f8d499a280afdad1db
levovix <xlevovix@gmail.com>
2025-09-02 15:38:57 +0300

    disable test on windows

--- tests/nimony/ffi/timportcnested.nim
+++ tests/nimony/ffi/timportcnested.nim
@@ -1,3 +1,5 @@
-type SockLen* {.importc: "socklen_t", header: "<sys/socket.h>".} = cuint
 
-var x: SockLen = 1.SockLen
+when defined(posix):
+  type SockLen* {.importc: "socklen_t", header: "<sys/socket.h>".} = cuint
+
+  var x: SockLen = 1.SockLen
