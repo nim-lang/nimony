51fae49d14892f9096379f07424658648d470193
Araq <rumpf_a@web.de>
2025-09-30 12:12:46 +0200

    added test case

---
+++ tests/nimony/closures/tcounter.nim
@@ -0,0 +1,13 @@
+
+import std/[syncio]
+
+proc createCounter(): (proc(): int {.closure.}) =
+  var count = 0
+  result = proc(): int {.closure.} =
+    inc count
+    return count
+
+let counter = createCounter()
+echo counter() # 1
+echo counter() # 2
+echo counter() # 3
