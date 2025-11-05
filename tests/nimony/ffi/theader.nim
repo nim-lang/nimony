083c6e5b229576d2cb863094af7d21603fcbe4b2
ringabout <43030857+ringabout@users.noreply.github.com>
2025-11-05 21:38:56 +0800

    fixes #1360; `importc` makes fields `exportc`

--- tests/nimony/ffi/theader.nim
+++ tests/nimony/ffi/theader.nim
@@ -3,3 +3,15 @@ type Bar1 {.importc: "Bar1", header: "${path}/bar1.h".} = object
 
 const header = "bar.h"
 type Bar2 {.importc: "Bar", header: header.} = object
+
+import std/assertions
+
+type
+  Color* {.bycopy, importc, header: header.} = object
+    r*: uint8
+    g*: uint8
+    b*: uint8
+    a*: uint8
+
+var color = Color(r: 8, g: 8, b: 8, a: 8)
+assert color.r == 8
