7e4876d71518fb8fa5b40a7988ec6a7262b8faed
Andreas Rumpf <rumpf_a@web.de>
2026-02-16 15:32:49 +0100

    closes #1609 (#1665)

--- tests/nimony/consteval/tmyops.nim
+++ tests/nimony/consteval/tmyops.nim
@@ -19,3 +19,19 @@ proc cantusesomething(): int =
 const s = cantusesomething()
 assert s == 42
 
+proc whileLoop(): int =
+  var i = 0
+  while i < 1:
+    inc i
+  42
+
+const s2 = whileLoop()
+assert s2 == 42
+
+proc forLoop(limit: int): int =
+  result = 0
+  for i in 1..limit:
+    result += i
+
+const s3 = forLoop(3)
+assert s3 == 6
