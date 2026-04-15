c04c586981c02f6bbb4386b2df636f793136a84c
blackmius <blackmius@gmail.com>
2026-04-15 21:29:52 +0300

    args test

--- tests/nimony/cps/tclosure.nim
+++ tests/nimony/cps/tclosure.nim
@@ -20,3 +20,17 @@ proc main2() =
     x()
 
 main2()
+
+proc a2(x: int): proc (q: int) {.passive,closure.} =
+    var b = x;
+    return proc (q: int) {.passive,closure.} =
+        b += q
+        echo "a2 ", b
+
+proc main3() =
+    var b = 0
+    let x = a2(10)
+    x(10)
+    x(10)
+
+main3()
