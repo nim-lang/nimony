46369a42df8d95c99a25cba15036d43a29a2f9c3
Gerke Max Preussner <gmpreussner@headcrash.industries>
2026-06-04 18:34:08 -0400

    Fixed bad merge

--- src/nimony/semdecls.nim
+++ src/nimony/semdecls.nim
@@ -934,10 +934,11 @@ proc semProcImpl(c: var SemContext; dest: var TokenBuf; it: var Item; kind: SymK
         let hasBody = it.n.kind != DotToken
         let redefMsg = checkRoutineRedefinition(c, dest, declStart, symId, kind, hasBody, info)
         if redefMsg.len > 0:
-          dest.buildTree StmtsS, info:
-            dest.buildTree ErrT, info:
-              dest.add dotToken(info)
-              dest.add strToken(pool.strings.getOrIncl(redefMsg), info)
+          dest.addParLe(StmtsS, info)
+          dest.buildTree ErrT, info:
+            dest.add dotToken(info)
+            dest.add strToken(pool.strings.getOrIncl(redefMsg), info)
+          dest.addParRi()
           skip it.n
         else:
           dest.takeTree it.n
