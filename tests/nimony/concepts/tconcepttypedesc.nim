6fd33b369c9bb0d8c9c8088afeaabbe334dac10b
Gerke Max Preussner <gmpreussner@headcrash.industries>
2026-06-08 13:30:33 -0400

    Added unit tests for interactions between concept and typedesc

---
+++ tests/nimony/concepts/tconcepttypedesc.nim
@@ -0,0 +1,12 @@
+import std/syncio
+
+type
+  Foo = concept
+    proc one(_: typedesc[Self]): Self
+    proc `+`(a, b: Self): Self
+
+# compiler must check against Foo, but not try to resolve T.one,
+# which would result in "Error: undeclared identifier: 'one'"
+
+template getTwo[T: Foo](t: typedesc[T]): T =
+  t.one() + one(t)
---
+++ tests/nimony/concepts/tconcepttypedescinst.nim
@@ -0,0 +1,33 @@
+import std/syncio
+
+type
+  Foo = concept
+    proc one(_: typedesc[Self]): Self
+    proc `+`(a, b: Self): Self
+
+# use concept in typedesc template
+template one(_: typedesc[int]): int = 1
+
+echo int.one()
+echo one(int)
+
+# use typedesc template in other typedesc template
+template getTwo[T: Foo](t: typedesc[T]): T =
+    t.one() + one(t)
+
+echo int.getTwo()
+echo getTwo(int)
+
+# use typedesc template in typedesc function
+func getThree[T: Foo](t: typedesc[T]): T =
+  t.getTwo() + t.one()
+
+echo int.getThree()
+echo getThree(int)
+
+# use typedesc template in function
+func addOne[T: Foo](a: T): T =
+  a + T.one()
+
+echo 3.addOne()
+echo addOne(3)
