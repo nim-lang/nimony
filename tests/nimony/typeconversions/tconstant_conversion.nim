f709827f8a9fc3a1aed2c40090b872cf9e65f4e9
Nico <nicolas.otremba@web.de>
2026-06-10 23:02:18 +0200

    Fix float and int literals not being passable to float32 parameters (#1942)
    Allow passing float and int literals to functions expecting float32 if
    the literal can be represented as a float32 without precision loss.

---
+++ tests/nimony/typeconversions/tconstant_conversion.nim
@@ -0,0 +1,14 @@
+import std/syncio
+
+proc printI32(a: int32) = echo a
+proc printI64(a: int64) = echo a
+proc printF32(a: float32) = echo a
+proc printF64(a: float64) = echo a
+
+printI32 1'i32 + 2
+printI64 1 + 3
+
+printF32 1.0'f32 + 4.5
+printF32 1.0'f32 + 5
+printF64 1.0 + 6.0
+printF64 1.0 + 7
