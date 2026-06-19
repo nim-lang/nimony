d35121b91c8aff4285455676cda839cf79aff752
Gerke Max Preussner <gmpreussner@headcrash.industries>
2026-06-19 15:45:41 -0400

    Better unit test for typedesc function const folding

--- tests/nimony/const/tconsttypedescdot.nim
+++ tests/nimony/const/tconsttypedescdot.nim
@@ -1,9 +1,23 @@
-# const distinct conversion from a typedesc dot call (e.g. Answer(int.fourtytwo))
-
-type
-  Answer = distinct int
-
 func fourtytwo*(x: typedesc[int]): int = 42
 
-const answer1 = Answer(42)
-const answer2 = Answer(int.fourtytwo)
+# Must work with built-in types
+
+const builtInAnswer = int.fourtytwo
+const builtInAnswer2 = int.fourtytwo * 2
+
+# Must work with objects
+
+type
+  ObjectAnswer = object
+    value: int
+
+const objectAnswer = ObjectAnswer(value: int.fourtytwo)
+const objectAnswer2 = ObjectAnswer(value: int.fourtytwo * 2)
+
+# Must work with distinct types
+
+type
+  DistinctAnswer = distinct int
+
+const distinctAnswer = DistinctAnswer(int.fourtytwo)
+const distinctAnswer2 = DistinctAnswer(int.fourtytwo * 2)
