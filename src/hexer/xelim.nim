d4435b37f670de8cfb0cb56d1a395c7f0b7b9d11
Tomohiro <gpuppur@gmail.com>
2026-07-09 18:09:25 +0900

    fixes #1948; allows passing non closure procs to closure proc type params (#2074)
    When non closure procs are passed to closure proc type parameters,
    `ToClosureX` is applied to the arguments in sigmatch.
    `ToClosureX` converts non closure procs to the same tuple types as
    closure procs but env field is nil.
    When closure procs are called and env is nil, it calls the proc without
    env.

--- src/hexer/xelim.nim
+++ src/hexer/xelim.nim
@@ -574,7 +574,7 @@ proc trCond(c: var Context; dest: var TokenBuf; n: var Cursor; tar: var Target; 
        MinussetX, MulsetX, XorsetX, EqsetX, LesetX, LtsetX,
        InsetX, CardX, EmoveX, DestroyX, DupX, CopyX, WasmovedX,
        SinkhX, TraceX, InternalTypeNameX, InternalFieldPairsX,
-       FailedX, IsX, EnvpX, KvX, NoExpr:
+       FailedX, IsX, EnvpX, KvX, ToClosureX, NoExpr:
       trExpr c, dest, n, tar
   else:
     trExpr c, dest, n, tar
@@ -1078,7 +1078,7 @@ proc trExpr(c: var Context; dest: var TokenBuf; n: var Cursor; tar: var Target) 
        PlussetX, MinussetX, MulsetX, XorsetX, EqsetX, LesetX,
        LtsetX, InsetX, CardX, EmoveX, DestroyX, DupX, CopyX,
        WasmovedX, SinkhX, TraceX, InternalTypeNameX,
-       InternalFieldPairsX, FailedX, IsX, EnvpX, KvX, NoExpr:
+       InternalFieldPairsX, FailedX, IsX, EnvpX, KvX, ToClosureX, NoExpr:
       case n.stmtKind
       of IfS:
         trIf c, dest, n, tar
