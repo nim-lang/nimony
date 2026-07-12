26d0efe7a57d2f43b677763980798317a5ecd72b
Antonis Geralis <43617260+planetis-m@users.noreply.github.com>
2026-07-12 13:10:35 +0300

    add: add smartcli to standard library (#2140)

---
+++ tests/nimony/stdlib/tsmartcli_slots.nim
@@ -0,0 +1,81 @@
+import std/[assertions, smartcli]
+
+var
+  nifcArgc {.importc: "cmdCount".}: int32
+  nifcArgv {.importc: "cmdLine".}: ptr UncheckedArray[cstring]
+
+block:
+  nifcArgc = 6
+  let cargv = [
+    cstring"backup",
+    cstring"--mode=delta",
+    cstring"--output=backup.log",
+    cstring"run",
+    cstring"src",
+    cstring"dst"
+  ]
+  nifcArgv = cast[ptr UncheckedArray[cstring]](cargv.addr)
+
+  let options = cliapp"""Backup v0.1
+Copies files to a target.
+
+Usage: backup [options] (run SOURCE DEST | version)
+
+Commands:
+  run SOURCE DEST  Start the backup
+  version  Display version and quit
+
+Arguments:
+  SOURCE  Source path
+  DEST    Destination path
+
+Options:
+  --mode=full|delta  Backup mode
+  --output=FILE      Log file
+  -v, --verbose      Enable verbose output
+  -h, --help         Show this help and exit"""
+
+  assert options.source == "src"
+  assert options.dest == "dst"
+  assert options.output == "backup.log"
+  assert $options.mode == "cliModeDelta"
+  assert $options.command == "cmdRun"
+
+block:
+  nifcArgc = 7
+  let cargv = [
+    cstring"backup",
+    cstring"run",
+    cstring"--mode=full",
+    cstring"src",
+    cstring"--output=after.log",
+    cstring"dst",
+    cstring"-v"
+  ]
+  nifcArgv = cast[ptr UncheckedArray[cstring]](cargv.addr)
+
+  let options = cliapp"""Backup v0.1
+Copies files to a target.
+
+Usage: backup [options] (run SOURCE DEST | version)
+
+Commands:
+  run SOURCE DEST  Start the backup
+  version  Display version and quit
+
+Arguments:
+  SOURCE  Source path
+  DEST    Destination path
+
+Options:
+  --mode=full|delta  Backup mode
+  --output=FILE      Log file
+  -v, --verbose      Enable verbose output
+  -h, --help         Show this help and exit"""
+
+  assert options.source == "src"
+  assert options.dest == "dst"
+  assert options.output == "after.log"
+  assert options.verbose
+  assert $options.mode == "cliModeFull"
+  assert $options.command == "cmdRun"
---
+++ tests/nimony/stdlib/tsmartcli_smoke.nim
@@ -0,0 +1,104 @@
+import std/[assertions, smartcli]
+
+var
+  nifcArgc {.importc: "cmdCount".}: int32
+  nifcArgv {.importc: "cmdLine".}: ptr UncheckedArray[cstring]
+
+block:
+  nifcArgc = 5
+  let cargv = [
+    cstring"greeter",
+    cstring"--output=out.txt",
+    cstring"-v",
+    cstring"greet",
+    cstring"input.txt"
+  ]
+  nifcArgv = cast[ptr UncheckedArray[cstring]](cargv.addr)
+
+  let options = cliapp"""Greeter v0.1
+This program greets.
+
+Usage: greeter [options] (greet INPUT | version)
+
+Commands:
+  greet INPUT  Greets NAME
+  version  Displays version and quits
+
+Arguments:
+  INPUT  Input file
+
+Options:
+  --mode=fast|slow  Output mode
+  --output=FILE     Output file
+  -v, --verbose     Enable verbose output
+  -h, --help        Show this help and exit"""
+
+  assert options.input == "input.txt"
+  assert options.output == "out.txt"
+  assert options.verbose
+  assert $options.command == "cmdGreet"
+
+block:
+  nifcArgc = 4
+  let cargv = [
+    cstring"greeter",
+    cstring"--mode=slow",
+    cstring"greet",
+    cstring"input.txt"
+  ]
+  nifcArgv = cast[ptr UncheckedArray[cstring]](cargv.addr)
+
+  let options = cliapp"""Greeter v0.1
+This program greets.
+
+Usage: greeter [options] (greet INPUT | version)
+
+Commands:
+  greet INPUT  Greets NAME
+  version  Displays version and quits
+
+Arguments:
+  INPUT  Input file
+
+Options:
+  --mode=fast|slow  Output mode
+  --output=FILE     Output file
+  -v, --verbose     Enable verbose output
+  -h, --help        Show this help and exit"""
+
+  assert options.input == "input.txt"
+  assert $options.mode == "cliModeSlow"
+
+block:
+  nifcArgc = 5
+  let cargv = [
+    cstring"greeter",
+    cstring"greet",
+    cstring"--output=late.txt",
+    cstring"input.txt",
+    cstring"-v"
+  ]
+  nifcArgv = cast[ptr UncheckedArray[cstring]](cargv.addr)
+
+  let options = cliapp"""Greeter v0.1
+This program greets.
+
+Usage: greeter [options] (greet INPUT | version)
+
+Commands:
+  greet INPUT  Greets NAME
+  version  Displays version and quits
+
+Arguments:
+  INPUT  Input file
+
+Options:
+  --mode=fast|slow  Output mode
+  --output=FILE     Output file
+  -v, --verbose     Enable verbose output
+  -h, --help        Show this help and exit"""
+
+  assert options.input == "input.txt"
+  assert options.output == "late.txt"
+  assert options.verbose
+  assert $options.command == "cmdGreet"
