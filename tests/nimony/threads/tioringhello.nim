f8a98d2cdcc58886c750ba3df69817c6f9defccc
blackmius <blackmius@gmail.com>
2026-07-28 14:27:04 +0300

    test

---
+++ tests/nimony/threads/tioringhello.nim
@@ -0,0 +1,9 @@
+import std/ioring
+import std/syncio
+
+var ring = initIoRing()
+var buf = "Hello world\n"
+discard ring.submitWrite(stdout.getFileHandle, buf.toCString, buf.len)
+var comps: array[16, IoCompletion]
+let n = ring.waitCompletions(comps)
+echo "written=", comps[0].result, " n=", n, " buf.len=", buf.len
