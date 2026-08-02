43f344db4a2c1dc868771ee2e58b4321b7b74087
demotomohiro <gpuppur@gmail.com>
2026-08-02 10:42:59 +0900

    updates test msgs and removes duplicated test

--- tests/nimony/calls/tcall_undeclared_proc_err.nim
+++
@@ -1,8 +0,0 @@
-# issue #1293
-# should prints both the error in the procedure name and errors in arguments
-
-undeclaredProc(
-  undeclaredVar1, undeclaredVar2)
-
-undeclaredTmpl:
-  proc foo() {.wrongPragma1: "", wrongPragma2: "".} = discard
